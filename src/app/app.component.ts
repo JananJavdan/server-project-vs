e22c09b79630256bedbfc37e7405bb37bbe65d15
JananJavdan <fatemeh.javdan@student.intecbrussel.be>
2024-06-18 16:00:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ServerService } from './service/server.service';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { response } from 'express';
+import { DataState } from './enum/data-state.enum';
+import { error } from 'console';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; 
 
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, JsonPipe, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'serverapp';
+export class AppComponent implements OnInit{
+  appState$: Observable<AppState<CustomResponse>>;
+
+ constructor(private serverService: ServerService) {}
+ 
+ ngOnInit(): void {
+  this.appState$ = this.serverService.servers$
+  .pipe(
+    map(response => ({
+      dataState: DataState.LOADED_STATE,
+      appData: response,
+      error: null
+    }) as AppState<CustomResponse>),
+    startWith({ 
+      dataState: DataState.LOADING_STATE, 
+      appData: null,
+      error: null
+    } as AppState<CustomResponse>),
+    catchError((error: string) => 
+      of({
+        dataState: DataState.ERROR_STATE,
+        appData: null,
+        error: error 
+      } as AppState<CustomResponse>)
+    )
+  );
+  }
+ 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
+import { HttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes'; 
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+      provideClientHydration(),
+      provideHttpClient(),
+    importProvidersFrom(HttpClient)]
 };
